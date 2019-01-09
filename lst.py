22c4d99196fdb8c289097cc0815280d7ff533b64
BasicAcid <david.tabarie@gmail.com>
2019-01-09 09:18:36 +0100

    initial commit

---
+++ lst.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+class Task(object):
+    def __init__(self,nb,time):
+        self.nb = nb
+        self.deadline = deadline
+        self.ctime = ctime
+
+tasks_objects = [
+    Task(1, 200, ),
+    Task(2, 500, ),
+    Task(3, 300, )
+]
+
+# sorted_list = sorted (tasks_objects, key=lambda my_task: my_task.time)
+
+def lst(tsk):
+    # A: arrving time
+    # C: computing time
+    # D: deadline (relative deadline)
+    # slack_time = deadline - computing_time
+
+    for each in tasks_objects:
+        # calculate slack_time = deadline - computing_time
+        for
+
+
+        start = time.clock()
+
+
+
+        elapsed = time.clock() - start
